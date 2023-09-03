6b9d2b46ec943c1c71db366e612e8f9a2b6ea3cb
fb0801 <fbhatti0801@gmail.com>
2023-09-03 19:47:13 +0100

    final touches and styling

--- react-chatgpt-clone/server.js
+++ react-chatgpt-clone/server.js
@@ -4,8 +4,10 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
+require('dotenv').config()
 
-const API_KEY = 
+
+const API_KEY = process.env.API_KEY
 
 app.post('/completions', async(req, res) => {
     const options = {
