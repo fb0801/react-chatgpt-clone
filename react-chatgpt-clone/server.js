1dff6e048ee1e76bca105cd943eef5ecb00fa6a3
fb0801 <fbhatti0801@gmail.com>
2023-09-03 16:09:35 +0100

    api setup

--- react-chatgpt-clone/server.js
+++ react-chatgpt-clone/server.js
@@ -5,4 +5,28 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const API_KEY = ''
+
+app.post('/completions', async(req, res) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json' 
+        }, 
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: 'How are you'}],
+            max_tokens: 100
+        })
+    }
+    try{
+        const response = await fetch('https://api.openapi.com/v1/chat/completions', options)
+        const data = await response.json()
+        res.send(data)
+    }catch (error) {
+        console.error(error)
+    }
+})
+
 app.listen(PORT, () => console.log('Server running on PORT' + PORT))
